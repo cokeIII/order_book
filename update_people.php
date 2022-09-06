213764fab0ff5b9e22ec146cc97aac6129b63c0b
cokeIII <coke2484@gmail.com>
2022-09-06 16:15:21 +0700

    f

--- update_people.php
+++ update_people.php
@@ -1,44 +1,46 @@
-<?php 
+<?php
 require_once "connect.php";
 header('Content-Type: text/html; charset=utf-8');
-function getDep($p_id){
+function getDep($p_id)
+{
     global $conn;
-    $sql = "select * people_pro pr
+    echo $sql = "select * people_pro pr
     inner join people_dep pd on pd.people_dep_id = pr.people_dep_id
     where pr.people_id = '$p_id' and pd.people_depgroup_id = 3
     ";
-    $res = mysqli_query($conn,$sql);
+    echo "<br>";
+    $res = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($res);
     return $row["people_dep_name"];
 }
 $sqlCheckP = "select * from people p
 where p.people_id not in(select pl.people_id from people_real pl)";
-$resCheckP = mysqli_query($conn,$sqlCheckP);
-while($rowCheckP = mysqli_fetch_array($resCheckP)){
+$resCheckP = mysqli_query($conn, $sqlCheckP);
+while ($rowCheckP = mysqli_fetch_array($resCheckP)) {
     echo "-----------------------INSERT-------------------------------<br>";
     $people_id = $rowCheckP["people_id"];
     $people_name = $rowCheckP["people_name"];
     $people_surname = $rowCheckP["people_surname"];
-    $people_dep_name = getDep($people_id );
+    $people_dep_name = getDep($people_id);
     $sqlAddP = "insert into people_real 
     (people_name,people_surname,dep_name)
     value('$people_name','$people_surname','$people_dep_name')
     ";
-    mysqli_query($conn,$sqlAddP);
+    mysqli_query($conn, $sqlAddP);
 }
 echo "-----------------------UPDATE-------------------------------<br>";
 $sqlR = "select * from people_real";
-$resR = mysqli_query($conn,$sqlR);
-while($rowR = mysqli_fetch_array($resR)){
+$resR = mysqli_query($conn, $sqlR);
+while ($rowR = mysqli_fetch_array($resR)) {
     $people_name = $rowR["people_name"];
     $people_surname = $rowR["people_surname"];
-    echo $sql = "select * from people where people_name like '%".$people_name."%' and people_surname like '%".$people_surname."%'";
+    echo $sql = "select * from people where people_name like '%" . $people_name . "%' and people_surname like '%" . $people_surname . "%'";
     echo "<br>";
-    $res = mysqli_query($conn,$sql);
+    $res = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($res);
     $people_id = $row["people_id"];
     $people_idR = $rowR["people_id"];
-    echo $sqlUp = "update people_real set people_id = '$people_id' where people_name like '%".$people_name."%' and people_surname like '%".$people_surname."%'";
-    mysqli_query($conn,$sqlUp);
+    echo $sqlUp = "update people_real set people_id = '$people_id' where people_name like '%" . $people_name . "%' and people_surname like '%" . $people_surname . "%'";
+    mysqli_query($conn, $sqlUp);
     echo "<br>";
 }
