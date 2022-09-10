65570adb91036d1f927b53e181167658b45cffc0
cokeIII <coke2484@gmail.com>
2022-09-10 14:43:00 +0700

    f

---
+++ updatePQ.php
@@ -0,0 +1,13 @@
+<?php
+require_once "connect.php";
+$sql = "select * from order_books o
+inner join book b on b.author_id = o.author_id and b.pub_id = o.pub_id and b.subject_id = o.subject_id_book and o.price = b.price and o.qty_page = b.qty_page
+";
+$res = mysqli_query($conn,$sql);
+while($row = mysqli_fetch_array($res)){
+    $order_id = $row["order_id"];
+    $price = $row["price"];
+    $qty_page = $row["qty_page"];
+    $sqlUp = "update order_books set price = '$price', qty_page = '$qty_page' where order_id = '$order_id'";
+    mysqli_query($conn,$sqlUp);
+}
