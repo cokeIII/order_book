d1e7e0d037811c1bbd6f904b06e90a7990e9ab41
cokeIII <coke2484@gmail.com>
2022-09-10 14:44:25 +0700

    f

--- updatePQ.php
+++ updatePQ.php
@@ -8,6 +8,6 @@ while($row = mysqli_fetch_array($res)){
     $order_id = $row["order_id"];
     $price = $row["price"];
     $qty_page = $row["qty_page"];
-    $sqlUp = "update order_books set price = '$price', qty_page = '$qty_page' where order_id = '$order_id'";
+    echo $sqlUp = "update order_books set price = '$price', qty_page = '$qty_page' where order_id = '$order_id'";
     mysqli_query($conn,$sqlUp);
 }
