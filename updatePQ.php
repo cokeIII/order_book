0675a7ab686f5c2c13501e0fd16ee6cf3abdbff7
cokeIII <coke2484@gmail.com>
2022-09-10 14:45:52 +0700

    f

--- updatePQ.php
+++ updatePQ.php
@@ -1,7 +1,8 @@
 <?php
 require_once "connect.php";
 $sql = "select * from order_books o
-inner join book b on b.author_id = o.author_id and b.pub_id = o.pub_id and b.subject_id = o.subject_id_book and o.price = b.price and o.qty_page = b.qty_page
+inner join book b on b.author_id = o.author_id and b.pub_id = o.pub_id and b.subject_id = o.subject_id_book
+where order_id != 868 
 ";
 $res = mysqli_query($conn,$sql);
 while($row = mysqli_fetch_array($res)){
